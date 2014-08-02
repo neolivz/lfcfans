b3ff35a14c3e7b365df5241331872d38f07144c3
Jishnu Viswanath <neolivz@NEOLIVZ.MINT>
2014-08-02 20:17:04 +0530

    Initial Setup

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('bkopApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
